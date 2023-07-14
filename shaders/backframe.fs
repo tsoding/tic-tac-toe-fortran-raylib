b7318e12756caaac42afea1451e4416c2ec8b069
rexim <reximkut@gmail.com>
2023-07-14 11:19:53 +0700

    Explaing the alpha = 1 hack

--- shaders/backframe.fs
+++ shaders/backframe.fs
@@ -13,5 +13,11 @@ out vec4 finalColor;
 
 void main() {
     vec4 color = texture(texture0, vec2(fragTexCoord.x, 1 - fragTexCoord.y));
-    finalColor = vec4(color.xyz, 1);
+    float alpha = 1;
+    // NOTE: fixing alpha to be always one is important for the backframe because
+    // of the particles. They are faded away by slowing animating their alpha
+    // towards 0. For some reason it affects the alpha of their fragments in the
+    // backframe texture resulting in particles getting darker instead of
+    // fading out.
+    finalColor = vec4(color.xyz, alpha);
 }
